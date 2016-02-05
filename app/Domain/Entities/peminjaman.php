1280cf7b5c40016966d8888160a18c06413d14d6
sam smith <you@yourdomain.example.com>
2016-02-05 16:07:49 +0700

    realasi paket data pada table identitas dan talbel buku ,bisa menampilkan lamaharinya

--- app/Domain/Entities/peminjaman.php
+++ app/Domain/Entities/peminjaman.php
@@ -1,24 +1,20 @@
 <?php
 namespace App\Domain\Entities;
-
 use Illuminate\Database\Eloquent\Model;
-
 class peminjaman extends Model
 {
     protected $table = 'peminjaman';
-    protected $fillable = ['id', 'id_buku', 'id_identitas', 'hari_lama', 'tanggal_pinjam', 'tanggal_kembali'];
-    protected $with = ['buku', 'identitas'];
+    protected $fillable = ['id','id_buku', 'id_identitas', 'lama_hari', 'tanggal_pinjam', 'tanggal_kembali'];
+    protected $with =['buku','identitas'];
 
 
-    public function buku()
-    {
-        return $this->belongsTo('App\Domain\Entities\Buku', 'id_buku');
+    public function buku(){
+        return $this->belongsTo('App\Domain\Entities\Buku','id_buku');
+    }
+    public function identitas(){
+        return $this->belongsTo('App\Domain\Entities\identitas','id_identitas');
     }
 
-    public function identitas()
-    {
-        return $this->belongsTo('App\Domain\Entities\identitas', 'id_identitas');
-    }
 
 
 }
